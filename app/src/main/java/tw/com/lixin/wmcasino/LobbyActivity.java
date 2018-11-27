c70023836596da6355b1b6d77c80ad23060021d5
hal0360 <atromoby@gmail.com>
2018-11-27 18:58:28 +0800

    new update

--- app/src/main/java/tw/com/lixin/wmcasino/LobbyActivity.java
+++ app/src/main/java/tw/com/lixin/wmcasino/LobbyActivity.java
@@ -1,7 +1,9 @@
 package tw.com.lixin.wmcasino;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import tw.com.atromoby.widgets.ItemsView;
 
 public class LobbyActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class LobbyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+
+        ItemsView itemsView = findViewById(R.id.itemsView);
+        itemsView.add(new TableHolder());
+        itemsView.add(new TableHolder());
+        itemsView.add(new TableHolder());
+        itemsView.add(new TableHolder());
+        itemsView.add(new TableHolder());
     }
+
+
 }
---
+++ app/src/main/java/tw/com/lixin/wmcasino/TableHolder.java
@@ -0,0 +1,20 @@
+package tw.com.lixin.wmcasino;
+
+import tw.com.atromoby.widgets.ItemHolder;
+
+public class TableHolder extends ItemHolder {
+
+    public TableHolder() {
+        super(R.layout.table_item);
+    }
+
+    @Override
+    public void onBind() {
+
+    }
+
+    @Override
+    public void onClean() {
+
+    }
+}
