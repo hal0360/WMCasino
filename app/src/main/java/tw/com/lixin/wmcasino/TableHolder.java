8421adba955d85d78410ecda4c4c60839904c6be
hal0360 <atromoby@gmail.com>
2018-12-14 18:46:09 +0800

    new update

--- app/src/main/java/tw/com/lixin/wmcasino/TableHolder.java
+++ app/src/main/java/tw/com/lixin/wmcasino/TableHolder.java
@@ -1,5 +1,6 @@
 package tw.com.lixin.wmcasino;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -11,8 +12,10 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import tw.com.atromoby.utils.Json;
 import tw.com.atromoby.widgets.ItemHolder;
 import tw.com.lixin.wmcasino.Tools.CasinoGrid;
+import tw.com.lixin.wmcasino.jsonData.CasinoData;
 import tw.com.lixin.wmcasino.jsonData.data.TableStage;
 
 public class TableHolder extends ItemHolder {
@@ -27,14 +30,67 @@ public class TableHolder extends ItemHolder {
     @Override
     public void onBind() {
 
+    }
+
+    @Override
+    public void onRecycle() {
+
+    }
+
+    @Override
+    public void onCreate() {
         CasinoGrid grid = findView(R.id.road_grid);
-        grid.setGrid(29, 6);
-        grid.insertImage(5,2, R.drawable.casino_roadplay);
+        grid.setGrid(28, 6);
+
         TextView gyuTxt = findView(R.id.gyu_shu);
         gyuTxt.setText("局数  " + stage.gameNo + " -- " + stage.gameNoRound);
         TextView numTxt = findView(R.id.table_num);
         numTxt.setText("00" + stage.groupID);
 
+        int xx = 0;
+        int yy = 0;
+
+        clicked(R.id.table_img,v->{
+            alert(Json.to(stage.historyArr));
+        });
+
+        clicked(R.id.table_grid,v->{
+            Intent intent = new Intent(getContex(), CasinoActivity.class);
+            getContex().startActivity(intent);
+        });
+
+
+        String asstxt = "";
+
+
+        for(int his: stage.historyArr){
+
+
+            if(xx < 28) {
+
+                List<Integer> twoIns = CasinoData.divide(his);
+                int val = twoIns.get(twoIns.size() - 1);
+                asstxt = asstxt + val + ",";
+
+                if(val == 1){
+                    grid.insertImage(xx,yy, R.drawable.bank_1);
+                }else if( val == 2){
+                    grid.insertImage(xx,yy, R.drawable.play_1);
+                }else if( val == 4){
+                    grid.insertImage(xx,yy, R.drawable.play_5);
+                }else if( val == 8){
+                    grid.insertImage(xx,yy, R.drawable.bank_2);
+                }else if( val == 16){
+                    grid.insertImage(xx,yy, R.drawable.play_2);
+                }
+                xx++;
+
+            }
+
+
+        }
+
+        gyuTxt.setText(asstxt);
     }
 
     //public static arrange
@@ -55,9 +111,4 @@ public class TableHolder extends ItemHolder {
         return powers;
     }
 
-    @Override
-    public void onClean() {
-
-
-    }
 }
