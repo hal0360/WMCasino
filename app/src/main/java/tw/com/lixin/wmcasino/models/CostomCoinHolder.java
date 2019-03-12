a4e192d33186ed48db47726be49beaf7e36170ed
hal0360 <atromoby@gmail.com>
2019-03-12 18:20:11 +0800

    bigggh ghh

--- app/src/main/java/tw/com/lixin/wmcasino/models/CostomCoinHolder.java
+++ app/src/main/java/tw/com/lixin/wmcasino/models/CostomCoinHolder.java
@@ -1,23 +1,41 @@
 package tw.com.lixin.wmcasino.models;
 
 import android.view.View;
+import android.widget.TextView;
 
 import tw.com.atromoby.widgets.ItemHolder;
 import tw.com.lixin.wmcasino.CasinoActivity;
 import tw.com.lixin.wmcasino.CoinHolder;
 import tw.com.lixin.wmcasino.R;
+import tw.com.lixin.wmcasino.Tools.NumberPadPopup;
 
 public class CostomCoinHolder extends ItemHolder {
 
     public int value, img_res;
     public boolean selected = false;
     public CoinHolder coinHolder;
+    public TextView display;
 
     public CostomCoinHolder() {
         super(R.layout.custom_coin_item);
-        this.value = 10;
+        this.value = 0;
         img_res = R.drawable.casino_item_chip;
         coinHolder = new CoinHolder(img_res,10);
+    }
+
+    public void setVal(int val){
+        this.value = val;
+        coinHolder = new CoinHolder(img_res,val);
+
+        if(!selected){
+
+            selected = true;
+            CasinoActivity act = (CasinoActivity) getContex();
+            act.curCoin.selected = false;
+            act.curCoin = coinHolder;
+            act.coinsView.refresh();
+
+        }
     }
 
     @Override
@@ -27,18 +45,14 @@ public class CostomCoinHolder extends ItemHolder {
         coin.setBackgroundResource(img_res);
         View chip = findViewById(R.id.chip);
 
-        /*
+        display = findViewById(R.id.display_txt);
+
         clicked(R.id.coin, v -> {
-            if(!selected){
 
-                selected = true;
-                CasinoActivity act = (CasinoActivity) getContex();
-                act.curCoin.selected = false;
-                act.curCoin = this;
-                act.coinsView.refresh();
+            new NumberPadPopup(getContex(),this).show();
 
-            }
-        });*/
+
+        });
 
         if(selected){
             chip.setBackgroundResource(R.drawable.outer_glow);
